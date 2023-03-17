b215e584b9fa7dffdb6cc4ed41cf76c97a33152b
dothedro <drooooofficial@gmail.com>
2023-03-17 21:41:20 +0800

    fix: some bugs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,25 @@
 const Home = () => {
     return (
-        <h2>Lorem ipsum dolor sit amet</h2>
+        <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <h2>Lorem ipsum dolor sit amet</h2>
+        </div>
     )
 }
 
